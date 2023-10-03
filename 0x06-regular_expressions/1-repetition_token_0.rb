28c33d71bb17b752524131fd624e5edb39a0fd85
Xframex <loukili.issmael@gmail.com>
2023-10-03 12:31:46 -0700

    Pattern for gready

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit(1)
+end
+
+pattern = /hbt*n/
+
+
+input_string = ARGV[0]
+
+
+matches = input_string.scan(pattern)
+puts matches.join
