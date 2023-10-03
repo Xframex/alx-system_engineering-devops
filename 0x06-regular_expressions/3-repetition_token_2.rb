6140d3ff5e2f4ecad34148dacc9a558956fbd8dc
Xframex <loukili.issmael@gmail.com>
2023-10-03 12:56:12 -0700

    a lot of pattern over ther

---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+if ARGV.length != 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit(1)
+end
+
+pattern = /hb?t*n/
+input_string = ARGV[0]
+matches = input_string.scan(pattern)
+puts matches.join
