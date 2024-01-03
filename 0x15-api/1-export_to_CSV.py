abf6ad1aa4ca008bf763ce90d2732c3c64e67e2c
Xframex <loukili.issmael@gmail.com>
2024-01-02 16:47:14 -0800

    fix task2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
-"""Gather data for another purpose"""
+"""get information about employee ID and convert to CSV format."""
+import csv
+import requests
+import sys
 
 if __name__ == "__main__":
-    import requests
-    from sys import argv
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    file_path = argv[1] + "_data.csv"
-    api_url = "https://jsonplaceholder.typicode.com"
-    user = requests.get('{}/users/{}'.format(api_url, argv[1])).json()
-    posts = requests.get('{}/posts/?userId={}'.format(api_url, argv[1])).json()
-    cm = requests.get('{}/comments/?postId={}'.format(api_url, argv[1])).json()
-
-    user_name = user.get('username')
-    with open(file_path, 'a') as f:
-        for post in posts:
-            f.write(
-                "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(
-                    argv[1],
-                    user_name,
-                    post.get('title'),
-                    post.get('body')
-                )
-            )
-
-        for comment in comments:
-            f.write(
-                "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(
-                    argv[1],
-                    user_name,
-                    comment.get('email'),
-                    comment.get('body')
-                )
-            )
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
