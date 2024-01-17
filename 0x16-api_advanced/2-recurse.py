8c114db69c1e581d6bb5be23cf19631d070a7e9f
Xframex <loukili.issmael@gmail.com>
2024-01-17 11:09:50 -0800

    recurse

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Hot recurse module"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Return a list of format json"""
+    if not subreddit:
+        return None
+    user_agent = {'User-Agent': 'Xframex'}
+    parameters = {'after': after, 'limit': 100}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers=user_agent, params=parameters)
+    if response.status_code not in range(200, 300):
+        return None
+    response_json = response.json()
+    after = response_json['data']['after']
+    elements = response_json['data']['children']
+    for element in elements:
+            hot_list.append(element['data']['title'])
+    if after is None:
+        return hot_list
+    else:
+        return (recurse(subreddit, hot_list, after))
